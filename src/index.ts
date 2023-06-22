f5aa0f14503ea166efb1ce409b15d10c0e81b62b
gracefullight <gracefullight.dev@gmail.com>
2023-06-22 09:46:22 +0900

    chore(deps): yarn to pnpm

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,11 @@ import { runServer } from "@verdaccio/node-api";
 import { config } from "dotenv";
 
 config({
-  path: resolve(__dirname, "../config/.env"),
+  path: resolve(__dirname, "..", ".env"),
 });
 
 (async () => {
-  const app = await runServer(
-    resolve(__dirname, "..", "config", "verdaccio.yml")
-  );
+  const app = await runServer(resolve(__dirname, "..", "verdaccio.yml"));
   app.listen(3000, () => {
     console.log("server started");
   });
